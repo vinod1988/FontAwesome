921dce966f94880b1668a93a4f1115fc5d55723f
Vinod Vishwakarma <vinod.vishwakarma@raweng.com>
2015-06-18 17:45:41 +0530

    # font awesome apply

--- FontAwesome/ViewController.h
+++ FontAwesome/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *testButton;
+@property (weak, nonatomic) IBOutlet UILabel *testLabel;
 
 @end
 
--- FontAwesome/ViewController.m
+++ FontAwesome/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "NSString+FontAwesome.h"
+#import "UIFont+FontAwesome.h"
+#import "UIImage+FontAwesome.h"
 
 @interface ViewController ()
 
@@ -17,6 +20,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    [self.testButton setTitle:[NSString fontAwesomeIconStringForIconIdentifier:@"fa-plus"] forState:UIControlStateNormal ];
+//    self.testButton.titleLabel.font = [UIFont fontWithName:kFontAwesomeFamilyName size:18];
+//
+
+    [self.testButton setTitle:[NSString fontAwesomeIconStringForEnum:FAPlus] forState:UIControlStateNormal ];
+    self.testButton.titleLabel.font = [UIFont fontWithName:kFontAwesomeFamilyName size:18];
+    
+    [self.testButton setTintColor:[UIColor grayColor]];
+
+
+    self.testLabel.text = [NSString fontAwesomeIconStringForIconIdentifier:@"fa-github"];
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@", [NSString fontAwesomeIconStringForIconIdentifier:@"fa-github"]]];
+    [attributedString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:16] range:NSMakeRange(1, [self.testLabel.text length] - 1)];
+    [attributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:kFontAwesomeFamilyName size:22] range:NSMakeRange(0, 1)];
+    [self.testLabel setAttributedText:attributedString];
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
